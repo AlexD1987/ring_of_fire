f32ece8be4769a29ba9c98043ab026f151472063
Alexander Dause <alexander.dause@web.de>
2023-11-16 20:23:39 +0100

    update card animation and player image

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,25 +1,38 @@
+import { Game } from './../../models/game';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-game',
-    templateUrl: './game.component.html',
-    styleUrls: ['./game.component.scss']
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-    cardAnimation = false;
-    constructor() {
+  cardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game();
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  selectCard() {
+    if (!this.cardAnimation) {
+        this.currentCard = this.game.stack.pop()!;
+    this.cardAnimation = true;
+    console.log(this.currentCard);
+
+
+    setTimeout(() => {
+        this.game.playedCard.push(this.currentCard);
+        this.cardAnimation = false;
+    }, 1500);
     }
-
-    ngOnInit(): void {
-
-    }
-
-    selectCard() {
-        this.cardAnimation = true;
-
-    }
-
-
-
+  }
 }
