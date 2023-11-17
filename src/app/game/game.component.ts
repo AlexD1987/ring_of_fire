6e58555db23ca53dc0b95d5383454950d2ef72ee
AlexD87 <alexander.dause@web.de>
2023-11-17 02:19:00 +0100

    add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,37 +2,37 @@ import { Game } from './../../models/game';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss'],
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-  cardAnimation = false;
-  currentCard: string = '';
-  game: Game = new Game();
+    cardAnimation = false;
+    currentCard: string = '';
+    game: Game = new Game();
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit(): void {
-    this.newGame();
-  }
-
-  newGame() {
-    this.game = new Game();
-    console.log(this.game);
-  }
-
-  selectCard() {
-    if (!this.cardAnimation) {
-        this.currentCard = this.game.stack.pop()!;
-    this.cardAnimation = true;
-    console.log(this.currentCard);
-
-
-    setTimeout(() => {
-        this.game.playedCard.push(this.currentCard);
-        this.cardAnimation = false;
-    }, 1500);
+    ngOnInit(): void {
+        this.newGame();
     }
-  }
+
+    newGame() {
+        this.game = new Game();
+        console.log(this.game);
+    }
+
+    selectCard() {
+        if (!this.cardAnimation) {
+            this.currentCard = this.game.stack.pop()!;
+            this.cardAnimation = true;
+            console.log(this.currentCard);
+
+
+            setTimeout(() => {
+                this.game.playedCard.push(this.currentCard);
+                this.cardAnimation = false;
+            }, 1500);
+        }
+    }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,36 +1,36 @@
 export class Game {
-  public players: string[] = [];
-  public stack: string[] = [];
-  public playedCard: string[] = [];
-  public currentPlayer: number = 0;
+    public players: string[] = ['Alex', 'Olga', 'Emilia', 'Micah'];
+    public stack: string[] = [];
+    public playedCard: string[] = [];
+    public currentPlayer: number = 0;
 
-  constructor() {
-    for (let i = 1; i < 14; i++) {
-      this.stack.push('Spades_' + i);
-      this.stack.push('Hearts_' + i);
-      this.stack.push('Clubs_' + i);
-      this.stack.push('Diamond_' + i);
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('Spades_' + i);
+            this.stack.push('Hearts_' + i);
+            this.stack.push('Clubs_' + i);
+            this.stack.push('Diamond_' + i);
+        }
+        shuffle(this.stack);
     }
-    shuffle(this.stack);
-  }
 }
 
 function shuffle(array: any[]) {
-  let currentIndex = array.length,
-    randomIndex;
+    let currentIndex = array.length,
+        randomIndex;
 
-  // While there remain elements to shuffle.
-  while (currentIndex > 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
 
-  return array;
+    return array;
 }
